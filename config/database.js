45e1a1ec247c12df1fd05ab599958e2cb0be8df0
Thiago Sem W <thiagosemw@gmail.com>
2020-10-06 23:06:17 -0300

    add database uri

---
+++ config/database.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+	uri: process.env.DATABASE_URI,
+	settings: {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+}
--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const compression = require('compression')
 const helmet = require('helmet')
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const databaseConfig = require('../config/database')
 
 const app = express()
 app.set('port', process.env.PORT || 5000)
@@ -17,5 +20,20 @@ app.use(express.json())
 if (app.get('env') === 'development') {
 	app.use(morgan('tiny'))
 }
+
+mongoose.connect(databaseConfig.uri, databaseConfig.settings)
+mongoose.connection.on('error', console.log)
+
+app.use('*', async (req, res, next) => {
+	res.status(404)
+	next(new Error('endpoint not exists'))
+})
+
+app.use(async (err, req, res, next) => {
+	const statusCode = res.statusCode || 500
+	res
+		.status(statusCode)
+		.json({ error: err.message })
+})
 
 module.exports = app
